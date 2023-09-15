d234878baf6ea2b6d2a61a5183f4ba4432b7c038
k8sbot <muralidhar.peddireddi@gmail.com>
2023-09-15 18:20:30 +0530

    json flatten

---
+++ json_flatten/locals.tf
@@ -0,0 +1,20 @@
+locals {
+    vnet = jsondecode(file("demo.json")).virtualNetworks
+
+    network = flatten([
+        for vnet_key , vnet_value in local.vnet : [
+            for snet_key , snet_value in vnet_value.subnets: {
+                    vnet_name = vnet_key
+                    vnet_address = vnet_value.address_prefix
+                    vnet_location = vnet_value.location
+                    subnet_name = snet_key
+                    subnet_address = snet_value.cidr_range
+            }
+            
+        ]
+    ])
+}
+
+output "network" {
+  value = local.network
+}
