f4748d23dc4efd4442768faf2b7e779054af9064
k8sbot <muralidhar.peddireddi@gmail.com>
2023-09-24 13:21:07 +0530

    added resource group

---
+++ json_flatten/v3/network.tf
@@ -0,0 +1,38 @@
+locals {
+    rsg_local = jsondecode(file("network.json")).Resourcegroup
+    network_local = jsondecode(file("network.json")).Virtualnetwork
+    
+    //resoucgroup01 for prod
+    rsg01 = flatten([for k , v in local.rsg_local : [
+
+        for key , val in v.production : {
+        resource_group = substr(format("%s-%s","prod",val.resource_group),0,12)
+        location = val.location
+      }
+    ]
+     ])
+
+    //resourcegroup2 for dr
+    rsg02 = flatten([for k , v in local.rsg_local : [
+
+        for key , val in v.dr : {
+        resource_group = substr(format("%s-%s","dr",val.resource_group),0,12)
+        location = val.location
+      }
+    ]
+     ])
+  //network
+    network = flatten([
+        for network_key , network in local.network_local : [
+            for sub_key , subnet_val in network.subnets : {
+              vnet_name = substr(format("%s-%s",network_key,"vnet"),0,15)
+              location = network.location
+              vnet_cidr = network.address_space
+              subnet_name = format("%s-%s",network.location,sub_key)
+              //cidrsubnet funtion need cidradress as string not list [], ""
+              subnet_address = cidrsubnet(join("",network.address_space),8,subnet_val.address_prefix)
+            } if sub_key != "delegated"
+        ]#if network_key != "dr"
+    ])
+}
+
---
+++ json_flatten/v3/outputs.tf
@@ -0,0 +1,10 @@
+output "rsg01_op" {
+  value = local.rsg01
+}
+output "rsg02_op" {
+  value = local.rsg02
+}
+
+output "network" {
+  value = local.network
+}
